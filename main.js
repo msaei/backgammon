d132e1fac45f3ae86fa712f48a19eaf071cd763d
mahdi saeikhalili <mahdi.saeikhalili@gmail.com>
2017-03-01 09:04:33 -0500

    board and checkers work nice

--- main.js
+++ main.js
@@ -6,11 +6,27 @@ function init() {
 
 	$('.checker').draggable({
 		revert: true ,
-		stack: '.checker'
+		stack: '.checker',
+		helper: 'original'
 	});
 
 	$(".room").droppable({ 
 		accept: '.checker',
-		hoverClass: 'hoverd'
+		hoverClass: 'hoverd',
+		drop: droped
 	});
+}
+
+function droped(event, ui) {
+	//ui.draggable.remove();
+	//var newElement = ui.draggable.clone();
+	var newElement = '<div class="' + ui.draggable.attr('class') + '"></div>';
+	ui.draggable.remove();
+	$(this).children().append(newElement);
+	$('.checker').draggable({
+		revert: true ,
+		stack: '.checker',
+		helper: 'original'
+	});
+
 }
