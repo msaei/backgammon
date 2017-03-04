70d096feac6813873c499d50a620bb699b9a1c2e
mahdi saeikhalili <mahdi.saeikhalili@gmail.com>
2017-03-03 23:27:44 -0500

    id and data added to every room properly

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var rooms = [0, 2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, -5, 0, 0, 0, 3, 0, 5, 0, 0
 $( init );
 
 function init() {
-	
+
 	initBoard();
 
 
@@ -24,6 +24,7 @@ function droped(event, ui) {
 	//ui.draggable.remove();
 	//var newElement = ui.draggable.clone();
 	var newElement = '<div class="' + ui.draggable.attr('class') + '"></div>';
+	alert($(this).data('num'));
 	ui.draggable.remove();
 
 	$(this).children().append(newElement);
@@ -42,16 +43,22 @@ function initBoard() {
 	$('<div class="bar"></div>').appendTo('#gameBoard');
 
 	//rooms 13-18 add to board
-	for(i=1; i<7; i++) {
-		$('<div class="room"></div>').appendTo('#gameBoard');
+	for(i=13; i<19; i++) {
+		$('<div class="room"><div class="top"></div></div>')
+		.data('num', i)
+		.attr('id', 'room'+i)
+		.appendTo('#gameBoard');
 	}
 
 	//top middle bar add to board
 	$('<div class="bar"></div>').appendTo('#gameBoard');
 
 	//rooms 19-24 add to board
-	for(i=1; i<7; i++) {
-		$('<div class="room"></div>').appendTo('#gameBoard');
+	for(i=19; i<25; i++) {
+		$('<div class="room"><div class="top"></div></div>')
+		.data('num', i)
+		.attr('id', 'room'+i)
+		.appendTo('#gameBoard');
 	}
 
 	//top right bar add to board
@@ -64,16 +71,22 @@ function initBoard() {
 	$('<div class="bar"></div>').appendTo('#gameBoard');
 
 	//room 7-12 add board
-	for(i=1; i<7; i++) {
-		$('<div class="room"></div>').appendTo('#gameBoard');
+	for(i=12; i>6; i--) {
+		$('<div class="room"><div class="bottom"></div></div>')
+		.data('num', i)
+		.attr('id', 'room'+i)
+		.appendTo('#gameBoard');
 	}
 
 	//middle bar add to board
 	$('<div class="bar"></div>').appendTo('#gameBoard');
 
 	//room 1-6 add to board
-	for(i=1; i<7; i++) {
-		$('<div class="room"></div>').appendTo('#gameBoard');
+	for(i=6; i>0; i--) {
+		$('<div class="room"><div class="bottom"></div></div>')
+		.data('num', i)
+		.attr('id', 'room'+i)
+		.appendTo('#gameBoard');
 	}
 
 	//bottom right bar add to board
